27acff8a817716c376fc80786d4732af1d6ea3c3
Felipe <prado.dias@hotmail.com>
2021-08-20 00:55:54 -0300

    Finalização adicionando funcionalidades do app e escolhendo a constrain como tela principal

--- app/src/main/java/com/example/appimc/MainActivity.kt
+++ app/src/main/java/com/example/appimc/MainActivity.kt
@@ -2,48 +2,73 @@ package com.example.appimc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import androidx.core.widget.doAfterTextChanged
+import androidx.core.widget.doOnTextChanged
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-    override fun onStart() {
-        super.onStart()
-        Log.w("Lifecycle", "Entrei no start = tela visível para o usuário")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.w("Lifecycle", "Entrei no resume = agora você pode interagir com a tela")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.w("Lifecycle", "Entrei no pause = a tela perdeu o foco, você não  mais pode interagir com a tela")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.w("Lifecycle", "Entrei no stop = tela não está mais visível, mas existe")
+        setListeners()
 
     }
 
-    override fun onRestart() {
-        super.onRestart()
-        Log.w("Lifecycle", "Entrei no restart = agora você pode interagir com a tela")
+  private fun setListeners(){
 
+      campoPeso?.doAfterTextChanged { text -> Toast.makeText(this
+          ,text.toString(),Toast.LENGTH_SHORT).show() }
+
+      campoAltura?.doAfterTextChanged { text -> Toast.makeText(this
+          ,text.toString(),Toast.LENGTH_SHORT).show() }
+
+      BotaoIMC.setOnClickListener{
+          calcular(campoAltura.text.toString(), campoPeso.text.toString())
+      }
+  }
+
+    private fun calcular(altura: String, peso: String){
+     //declarando variáveis
+
+        val altura = altura.toFloatOrNull()
+        val peso = peso.toFloatOrNull()
+
+        //checando nulidade
+        if (peso != null && altura != null){
+            //fazendo a função e alterando na view
+            val imc = peso / (altura * altura)
+
+            if (imc < 18.5){
+                mensagem01.text = "Seu IMC é %.2f - Abaixo do peso".format(imc)
+
+            }
+            if (imc > 18.5 && imc < 24.9){
+                mensagem01.text = "Seu IMC é %.2f - Peso ideal".format(imc)
+
+            }
+            if (imc > 25.0 && imc < 29.9){
+                mensagem01.text = "Seu IMC é %.2f - Levemente acima do peso".format(imc)
+
+            }
+            if (imc > 30.0 && imc < 34.9){
+                mensagem01.text = "Seu IMC é %.2f - Obesidade grau I".format(imc)
+
+            }
+            if (imc > 35.0 && imc < 39.9){
+                mensagem01.text = "Seu IMC é %.2f - Obesidade grau II".format(imc)
+
+            }
+            if (imc > 40.0){
+                mensagem01.text = "Seu IMC é %.2f - Obesidade grau III".format(imc)
+
+            }
+        }
     }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.w("Lifecycle", "Entrei no destroy = agora você pode interagir com a tela")
-    }
-
-
-
 
 }
+
+
+
